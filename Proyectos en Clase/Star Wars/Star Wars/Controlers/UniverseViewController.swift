ee292ce7de9067ec6c1905acac0cffa5643f24be
master <master@dib-e026-mac01k.dib.upv.es>
2021-01-15 20:37:55 +0100

    Actualizo StarWars

---
+++ Proyectos en Clase/Star Wars/Star Wars/Controlers/UniverseViewController.swift
@@ -0,0 +1,101 @@
+//
+//  UniverseViewController.swift
+//  Star Wars
+//
+//  Created by master on 15/01/2021.
+//  Copyright © 2021 master. All rights reserved.
+//
+
+import UIKit
+
+class UniverseViewController: UITableViewController {
+    
+    let model : StarWarsUniverse
+    
+    init(model: StarWarsUniverse){
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+        self.title = "Star Wars"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Numeo de afiliaciones
+        return model.affiliationCount
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Personajes que hay en cada seccion
+        return model.characterCount(forAffiliation: getAffiliation(forSection: section))
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // cabecera de cada seccion
+        return ""
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellId = "StarWarsIdCell"
+        
+        let char = character(forIndexPath: indexPath)
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+        }
+        
+        //Sincronizacion personaje y celda
+        
+        cell?.imageView?.image = char.photo
+        cell?.textLabel?.text = char.alias
+        cell?.detailTextLabel?.text = char.name
+        
+        return cell!
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // pasar al detalle
+    }
+    
+    func getAffiliation(forSection section: Int) -> StarWarsAffiliation {
+        var aff: StarWarsAffiliation = .unknown
+        
+        switch section {
+        case 0:
+            aff = .galacticEmpire
+        case 1:
+            aff = .rebelAlliance
+        case 2:
+            aff = .firstOrder
+        case 3:
+            aff = .jabbaCriminalEmpire
+        default:
+            aff = .unknown
+        }
+        
+        return aff
+    }
+    
+    func character(forIndexPath indexPath : IndexPath) -> StarWarsCharacter {
+        return model.character(atIndex: indexPath.row, forAffiliation: getAffiliation(forSection: indexPath.section))
+    }
+
+
+
+}
---
+++ Proyectos en Clase/Star Wars/Star Wars/Models/StarWarsUniverse.swift
@@ -0,0 +1,77 @@
+//
+//  StarWarsUniverse.swift
+//  Star Wars
+//
+//  Created by master on 15/01/2021.
+//  Copyright © 2021 master. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StarWarsUniverse {
+
+    //Typealias
+
+typealias StarWarsArray = [StarWarsCharacter]
+typealias StarWarsDictionary = [StarWarsAffiliation : StarWarsArray]
+    
+    var dict : StarWarsDictionary = StarWarsDictionary()
+    
+    init(character chars: StarWarsArray){
+        dict = makeEmptyAffiliations()
+        
+        for each in chars {
+            dict[each.affiliation]?.append(each)
+            
+        }
+    }
+    
+    // Responder al Data Source
+    
+    var affiliationCount : Int {
+        get {
+            return dict.count
+        }
+    }
+    
+    // Cuantas filas en cada seccion
+    func characterCount(forAffiliation affiliation: StarWarsAffiliation) -> Int {
+        //return dict[affiliation]!.count
+        
+        guard let count = dict[affiliation]?.count else{
+            return 0
+        }
+        
+        return count
+    }
+    
+    // Para rellenar en cada numero de fila de cada seccion el personaje que corresponda
+    
+    func character(atIndex index:Int, forAffiliation affiliation: StarWarsAffiliation) -> StarWarsCharacter{
+        
+        let chars = dict[affiliation]!
+        let char = chars[index]
+        
+        return char
+    }
+    
+    // Nombre de la seccion
+    func affiliationName(_ affiliation : StarWarsAffiliation) -> String{
+        return affiliation.rawValue
+    }
+    
+    func makeEmptyAffiliations() -> StarWarsDictionary {
+        var d = StarWarsDictionary()
+        
+        d[.rebelAlliance] = StarWarsArray()
+        d[.galacticEmpire] = StarWarsArray()
+        d[.jabbaCriminalEmpire] = StarWarsArray()
+        d[.firstOrder] = StarWarsArray()
+        d[.unknown] = StarWarsArray()
+        
+        return d
+        
+    }
+
+}
